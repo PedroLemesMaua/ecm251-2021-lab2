02891e1e523bf4024f3e9df159815dcde52492ea
PedroLemesMaua <79260276+PedroLemesMaua@users.noreply.github.com>
2021-03-15 10:14:25 -0300

    criação do construtor

---
+++ Aula05/src/Lemes/Pedro/Main.java
@@ -0,0 +1,12 @@
+package Lemes.Pedro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Sistema sistema = new Sistema();
+        sistema.executar();
+    }
+}
+
+
+
---
+++ Aula05/src/Lemes/Pedro/Sistema.java
@@ -0,0 +1,31 @@
+package Lemes.Pedro;
+
+import java.util.Scanner;
+
+public class Sistema {
+    private boolean executarSistema;
+    private Scanner scanner;
+
+    public void executar(){
+        int opcao;
+        while(executarSistema){
+            exibirMenu();
+            opcao = scanner.nextInt();
+        }
+    }
+
+    public Sistema() {
+        this.executarSistema = true;
+        this.scanner = new Scanner(System.in);  //Cria um scanner para o teclado (entrada padrão)
+    }
+
+    private void exibirMenu() {
+        System.out.println("Bem vindo ao MauaBank");
+        System.out.println("1 - Visualizar Saldo");
+        System.out.println("2 - Depositar dinheiro");
+        System.out.println("3 - Sacar dinheiro");
+        System.out.println("4 - Transferir dinheiro");
+        System.out.println("5 - Pagar conta(título)");
+        System.out.println("0 - Encerrar Sistema");
+    }
+}
