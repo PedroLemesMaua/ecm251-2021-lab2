8bd6bbe69cb35155dc7372a96c05ff7f3227ac06
PedroLemesMaua <79260276+PedroLemesMaua@users.noreply.github.com>
2021-03-18 09:12:32 -0300

    ValidadorDeCpf

---
+++ ValidadordeCPF/ValidadordeCPF/src/Lemes/Pedro/Main.java
@@ -0,0 +1,9 @@
+package Lemes.Pedro;
+
+public class Main {
+    public static void main(String[] args){
+            String cpf = "12345678912";
+            System.out.println("Cpf valido: " + ValidadorCPF.validarCpf(cpf));
+        }
+}
+
