fc036d3b3052e8f02abc909dc9d56cc5f5d84354
PedroLemesMaua <79260276+PedroLemesMaua@users.noreply.github.com>
2021-04-05 20:02:43 -0300

    melhoria

---
+++ Teste T1/src/Lemes/Pedro/Contas.java
@@ -0,0 +1,12 @@
+package Lemes.Pedro;
+
+public class Contas {
+    //Atributos
+    private String idConta;
+    private int saldo;
+
+    //Métodos
+    public Usuarios(String idConta, int saldo) {
+        this.idConta = new Contas(idConta);
+        this.saldo = saldo;
+    }
---
+++ Teste T1/src/Lemes/Pedro/Usuarios.java
@@ -0,0 +1,15 @@
+package Lemes.Pedro;
+
+
+public class Usuarios {
+    //Atributos
+    private String nome;
+    private int senha;
+    private String email;
+
+    //Métodos
+    public Usuarios(String nome, int senha, String email) {
+        this.nome = new Usuarios(nome);
+        this.senha = senha;
+        this.email = email;
+    }
